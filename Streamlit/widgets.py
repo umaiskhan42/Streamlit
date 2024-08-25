45ca222d4783e88497d8af55c0fcd5a197fc3e64
Umais Khan <omaisk.me42ceme@student.nust.edu.pk>
2024-08-25 17:03:55 +0500

    upload files

--- Streamlit/widgets.py
+++ Streamlit/widgets.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 st.title("Streamlit Text Input")
 name=st.text_input("Enter your name")
 
@@ -13,3 +14,8 @@ st.write(f"Your gender is {choice}")
 
 if name:
     st.write(f"Hello {name}")
+
+uploaded_file=st.file_uploader("Choose a csv file: ",type="csv")
+if uploaded_file is not None:
+    df=pd.read_csv(uploaded_file)
+    st.write(df)
