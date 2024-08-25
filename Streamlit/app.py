50658388502bd9646b5f9787bd4b7ede04d65f74
Umais Khan <omaisk.me42ceme@student.nust.edu.pk>
2024-08-25 16:33:33 +0500

    Dataframe display

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -4,3 +4,18 @@ import pandas as pd
 
 #Title of app
 st.title("Streamlit app")
+
+# Display simple text
+
+st.write("This is the text")
+
+# Create a dataframe
+
+df=pd.DataFrame({
+    'Column A': [1,2,3,4],
+    'Column B': [3,5,7,8]
+})
+
+# Display the dataframe
+st.write("here is the dataframe")
+st.write(df)
