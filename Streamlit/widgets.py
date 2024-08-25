4ffb3170d5aa968ba5286cde797abf22afcfac5d
Umais Khan <omaisk.me42ceme@student.nust.edu.pk>
2024-08-25 16:54:30 +0500

    added selectbox

--- Streamlit/widgets.py
+++ Streamlit/widgets.py
@@ -5,5 +5,11 @@ name=st.text_input("Enter your name")
 age=st.slider("write you age",0,100,5)
 
 st.write(f"Your age is {age}")
+
+options=['male','female']
+choice=st.selectbox("choose your gender",options)
+
+st.write(f"Your gender is {choice}")
+
 if name:
     st.write(f"Hello {name}")
