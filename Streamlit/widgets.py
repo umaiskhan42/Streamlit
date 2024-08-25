8196d8fa8fa2b1b267b0ca2fc7b3107f1608f66e
Umais Khan <omaisk.me42ceme@student.nust.edu.pk>
2024-08-25 16:52:23 +0500

    slider added

--- Streamlit/widgets.py
+++ Streamlit/widgets.py
@@ -2,5 +2,8 @@ import streamlit as st
 st.title("Streamlit Text Input")
 name=st.text_input("Enter your name")
 
+age=st.slider("write you age",0,100,5)
+
+st.write(f"Your age is {age}")
 if name:
     st.write(f"Hello {name}")
